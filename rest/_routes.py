6ea5dc320d70f42bd1cdf0b920021a2e1383f5c8
DKAC <daniel.klink@cs.rwth-aachen.de>
2021-12-14 11:24:59 +0100

    Update _routes.py

--- rest/_routes.py
+++ rest/_routes.py
@@ -12,7 +12,7 @@ def initialize_routes(api):
     api.add_resource(UserApi, '/user/<userid>')
     api.add_resource(ClanApi, '/clan/<oid>')
     api.add_resource(ClansApi, '/clans')
-    api.add_resource(MatchApi, '/matches/<oid>')    
+    api.add_resource(MatchApi, '/match/<oid>')    
     api.add_resource(MatchesApi, '/matches')    
     api.add_resource(ScoresApi, '/scores')
     api.add_resource(ScoreApi, '/score/<oid>')
