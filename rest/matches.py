83f0bc32e32d5e8ac960e6e50a228b5b1002c3de
DKAC <daniel.klink@cs.rwth-aachen.de>
2021-12-21 11:04:43 +0100

    Update matches.py

--- rest/matches.py
+++ rest/matches.py
@@ -54,23 +54,31 @@ class MatchesApi(Resource):
         try:
             select = request.args.get('select')
             match_id = request.args.get('match_id')
+            clan_id = request.args.get('clan_id')
             clan1_id = request.args.get('clan1_id')
             coop1_id = request.args.get('coop1_id')
             clan2_id = request.args.get('clan2_id')
             coop2_id = request.args.get('coop2_id')
             conf1 = request.args.get('conf1')
             conf2 = request.args.get('conf2')
+            date = request.args.get('date')
+            date_from = request.args.get('date_from')
+            date_to = request.args.get('date_to')
 
             fields = select.split(',') if select != None else []
                 
             where = Q()
             if match_id != None: where &= Q(match_id=match_id)
+            if clan_id != None: where &= (Q(clan1_id=clan_id) | Q(clan2_id=clan_id))
             if clan1_id != None: where &= Q(clan1_id=clan1_id)
             if coop1_id != None: where &= Q(coop1_id=coop1_id)
             if clan2_id != None: where &= Q(clan2_id=clan2_id)
             if coop2_id != None: where &= Q(coop2_id=coop2_id)
             if conf1 != None: where &= Q(conf1=conf1)
             if conf2 != None: where &= Q(conf2=conf2)
+            if date != None: where &= Q(date=date)
+            if date_from != None: where &= Q(date__gte=date_from)
+            if date_to != None: where &= Q(date__lte=date_to)
 
             matches = Match.objects(where).only(*fields)
                         
