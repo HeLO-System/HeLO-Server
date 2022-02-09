989a5e348f95f8611fb254acd8ef9c44ad2c621c
Soxxes <hscschwimmermarc@t-online.de>
2022-02-09 22:50:10 +0100

    moved coop calc tests to another repo

--- logic/calculations.py
+++ logic/calculations.py
@@ -113,34 +113,3 @@ def get_coop_scores(clan_scores1: list, clan_scores2: list, caps1: int, caps2: i
     clan_scores2 = [round(cs + part * gain2) for cs, part in zip(clan_scores2, weights2)]
 
     return clan_scores1, clan_scores2
-
-
-if __name__ == "__main__":
-    # some scores
-    clan1 = 513
-    clan2 = 683
-    clan3 = 711
-    clan4 = 578
-    clan5 = 604
-
-    coop1 = [clan2, clan3]          # in Germany we say: "Die guten ins Töpfchen ..."
-    coop2 = [clan1, clan4, clan5]   # " ... die schlechten ins Kröpfchen"
-
-    player_distribution1 = [30, 20]
-    player_distribution2 = [5, 25, 20]
-
-    # game result
-    caps1 = 5
-    caps2 = 0
-
-    # test a: with distributions
-    new1a, new2a = get_coop_scores(coop1, coop2, caps1, caps2,
-                                    player_dist1=player_distribution1,
-                                    player_dist2=player_distribution2)
-
-    # test b: without distributions
-    new1b, new2b = get_coop_scores(coop1, coop2, caps1, caps2)
-
-    print("------------------------------------")
-    print(f"test a: {new1a}, {new2a}")
-    print(f"test b: {new1b}, {new2b}")
