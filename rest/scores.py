9dc1b81d4fff820c23e843e958c11ad650cf5cee
Soxxes <50017993+Soxxes@users.noreply.github.com>
2022-04-13 14:41:55 +0200

    queries for scores

--- rest/scores.py
+++ rest/scores.py
@@ -3,9 +3,10 @@ from flask import request
 from flask_restful import Resource
 from flask_jwt_extended import jwt_required
 from mongoengine import Q
+from mongoengine.errors import LookUpError
 
 from models.score import Score
-from ._common import get_response, handle_error
+from ._common import get_response, handle_error, empty, admin_required
 
 
 class ScoreApi(Resource):
@@ -21,7 +22,7 @@ class ScoreApi(Resource):
 
     # update scores by object id
     # will be deleted soon
-    @jwt_required()
+    @admin_required()
     def put(self, oid):
         try:
             scores = Score.objects.get(id=oid)        
@@ -34,7 +35,7 @@ class ScoreApi(Resource):
             return handle_error(f"error updating scores in database, scores not found by oid: {oid}")
 
     # update scores by object id
-    @jwt_required()
+    @admin_required()
     def delete(self, oid):
         try:
             scores = Score.objects.get(id=oid)        
@@ -54,22 +55,44 @@ class ScoresApi(Resource):
     # get all or filtered by clan tag
     def get(self):
         try:
-            select = request.args.get('select')
-            clan = request.args.get('clan')        
+            select = request.args.get("select")
+            clan = request.args.get("clan_id")
+            match_id = request.args.get("match_id")
+            score = request.args.get("score")
+            score_from = request.args.get("score_from")
+            score_to = request.args.get("score_to")
 
-            where = Q(clan=clan) if clan != None else Q()
-            fields = select.split(',') if select != None else []
+            fields = select.split(',') if select is not None else []
+
+            filter = Q()
+
+            if not empty(clan): filter &= Q(clan=clan)
+            if not empty(match_id): filter &= Q(match_id__icontains=match_id)
+            if not empty(score): filter &= Q(score=score)
+            if not empty(score_from): filter &= Q(score__gte=score_from)
+            if not empty(score_to): filter &= Q(score__lte=score_from)
             
-            scores = Score.objects(where).only(*fields)
+            total = Score.objects(filter).only(*fields).count()
+            scores = Score.objects(filter).only(*fields)
 
-            return get_response(scores)
+            res = {
+                "total": total,
+                "items": scores.to_json_serializable()
+            }
+
+            return get_response(res)
+
+        except LookUpError:
+            return {"error": f"cannot resolve field 'select={select}'"}, 400
+
+
         except:
             return handle_error("Error getting scores from database")
         
 
     # add new scores
     # will be deleted soon
-    @jwt_required()
+    @admin_required()
     def post(self):
         try:
             score = Score(**request.get_json())
