da947160c26be84d530d17ab04f80147ac11418d
Soxxes <hscschwimmermarc@t-online.de>
2022-08-06 19:38:38 +0200

    updated: factors for console simualtions

--- schemas/request_schemas.py
+++ schemas/request_schemas.py
@@ -20,7 +20,7 @@ class ConsoleSimulationsSchema(Schema):
     caps2 = fields.Integer(required=True, validate=Range(min=0, max=5, min_inclusive=True, max_inclusive=True))
     player_dist1 = fields.List(fields.Integer(validate=Range(min=0, max=50, min_inclusive=True, max_inclusive=True)))
     player_dist2 = fields.List(fields.Integer(validate=Range(min=0, max=50, min_inclusive=True, max_inclusive=True)))
-    factor = fields.Float(validate=OneOf([0.3, 0.6, 2.0, 2.4]))
+    factor = fields.Float(validate=OneOf([0.5, 0.8, 1.0, 1.2]))
     players1 = fields.Integer(validate=Range(min=3, max=50, max_inclusive=True))
     players2 = fields.Integer(validate=Range(min=3, max=50, max_inclusive=True))
     randoms1 = fields.Integer(validate=Range(min=3, max=50, max_inclusive=True))
