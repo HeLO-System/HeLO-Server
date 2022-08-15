f2cfce4a6b7ae19b567f64ddf4d5502950e9df8d
Florian <florian.schmidt.welzow@t-online.de>
2022-08-15 17:23:24 +0200

    Some documentation for local setup

--- helo-server.py
+++ helo-server.py
@@ -1,22 +1,24 @@
 # HeLO.py
-import json, os, logging
+import json
+import logging
+import os
+
 from flask import Flask, render_template, jsonify
 from flask_bcrypt import Bcrypt
-from flask_discord import DiscordOAuth2Session
-from flask_restful import Api
 from flask_jwt_extended import JWTManager
+from flask_restful import Api
 
+from database._db import initialize_db
 from discord.auth import initialize_discord_auth
 from rest._routes import initialize_routes
-from database._db import initialize_db
-
 
 # init components
-logging.basicConfig(encoding='utf-8', level=logging.INFO, format=f"%(filename)20s:%(lineno)-3s - %(funcName)-30s %(message)s")
+logging.basicConfig(encoding='utf-8', level=logging.INFO,
+                    format=f"%(filename)20s:%(lineno)-3s - %(funcName)-30s %(message)s")
 
 app = Flask(__name__)
 
-DB = { 
+DB = {
     'USERNAME': os.environ.get('DB_USERNAME'),
     'PASSWORD': os.environ.get('DB_PASSWORD'),
     'HOST': os.environ.get('DB_HOST'),
@@ -48,6 +50,7 @@ jwt = JWTManager(app)
 # needs to be true for custom error messages
 app.config["PROPAGATE_EXCEPTIONS"] = True
 
+
 # custom error message for wrong JWTs
 @jwt.invalid_token_loader
 def invalid_token_callback(s):
@@ -57,10 +60,9 @@ def invalid_token_callback(s):
 # home page: offer some explanation how to use the API
 @app.route('/')
 def home():
-    return render_template('home.html', apis = json.loads(open('static/apis.json', 'r').read())), 200
+    return render_template('home.html', apis=json.loads(open('static/apis.json', 'r').read())), 200
 
 
 # start app
 if __name__ == "__main__":
     app.run()
-
